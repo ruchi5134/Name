b29e81a2a7b254d4c182fa1427855419cb147412
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2024-08-18 12:31:36 +0530

    Update __main__.py

--- PURVIMUSIC/__main__.py
+++ PURVIMUSIC/__main__.py
@@ -50,12 +50,12 @@ async def init():
         pass
     await PURVI.decorators()
     LOGGER("PURVIMUSIC").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗗𝗛𝗣𝗥☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗦𝗬𝗦𝗧𝗨𝗠𝗠☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("PURVIMUSIC").info("𝗦𝗧𝗢𝗣 𝗛𝗘𝗘𝗥 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
+    LOGGER("PURVIMUSIC").info("𝗦𝗧𝗢𝗣 𝗦𝗬𝗦𝗧𝗘𝗠 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
 
 
 if __name__ == "__main__":
