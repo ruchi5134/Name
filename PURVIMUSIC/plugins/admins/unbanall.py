8fa4bb59c4f50efb8bf3f377fc1382ba203c1328
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2024-11-20 11:53:09 +0530

    ᴠɪᴋᴋʏ

--- PURVIMUSIC/plugins/admins/unbanall.py
+++ PURVIMUSIC/plugins/admins/unbanall.py
@@ -4,7 +4,7 @@ from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from PURVIMUSIC.utils.purvi_ban import admin_filter
 
-BOT_ID = "6974730627"
+BOT_ID = "7548798871"
 
 @app.on_message(filters.command("unbanall") & admin_filter)
 async def unban_all(_, msg):
