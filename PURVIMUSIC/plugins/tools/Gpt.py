34a419f83b1fa77f26d1d60b00cb5b40f93b233e
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2024-12-18 20:44:45 +0530

    Update Gpt.py

--- PURVIMUSIC/plugins/tools/Gpt.py
+++ PURVIMUSIC/plugins/tools/Gpt.py
@@ -69,7 +69,7 @@ async def chat_gpt(bot, message):
                     if "choices" in response_data and len(response_data["choices"]) > 0:
                         result = response_data["choices"][0]["message"]["content"]
                         await message.reply_text(
-                            f"{result} \n\nᴛʜᴀɴᴋs ғᴏʀ ᴜsɪɴɢ ᴍᴇ 😍",
+                            f"{result}",
                             reply_markup=InlineKeyboardMarkup(
                         [
                             [
