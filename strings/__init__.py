4bce2d5e530fb3a26abcff6e1fa003cab1f41699
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2024-12-18 18:26:43 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -32,8 +32,7 @@ for filename in os.listdir(r"./strings/langs/"):
     except:
         print("There is some issue with the language file inside bot.")
         exit()
-
-except Exception as e:
+        except Exception as e:
                 import traceback 
                 print(f"Error loading {filename}: {e}")
                 traceback.print_exc()
