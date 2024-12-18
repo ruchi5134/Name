8c3191f670f3856a6be65c7b69cca9b9c27d1909
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2024-12-18 19:55:15 +0530

    Update images.py

--- PURVIMUSIC/plugins/yumi/images.py
+++ PURVIMUSIC/plugins/yumi/images.py
@@ -1,53 +1,28 @@
-import os
-import shutil
-from re import findall
-from bing_image_downloader import downloader
-from pyrogram import Client, filters
-from pyrogram.types import InputMediaPhoto, Message
+from pyrogram.types import InputMediaPhoto
+from config import BANNED_USERS
 from PURVIMUSIC import app
+from pyrogram import filters
+from TheApi import api
 
-@app.on_message(filters.command(["img", "image"], prefixes=["/", "!"]))
-async def google_img_search(client: Client, message: Message):
-    chat_id = message.chat.id
 
+@app.on_message(filters.command(["image"], prefixes=["/", "!", "."]) & ~BANNED_USERS)
+async def image_from_bing(_, message):
+    if len(message.command) < 2 and not message.reply_to_message:
+        return await message.reply_text("**ɢɪᴠᴇ ɪᴍᴀɢᴇ ɴᴀᴍᴇ ғᴏʀ sᴇᴀʀᴄʜ 🔍**")
+
+    if message.reply_to_message and message.reply_to_message.text:
+        query = message.reply_to_message.text
+    else:
+        query = " ".join(message.command[1:])
+
+    messagesend = await message.reply_text("**🔍 sᴇᴀʀᴄʜɪɴɢ ғᴏʀ ɪᴍᴀɢᴇs...**")
+
+    media_group = []
+    for url in api.bing_image(query, 6):
+        media_group.append(InputMediaPhoto(media=url))
+    await messagesend.edit(f"**ᴜᴘʟᴏᴀᴅɪɴɢ...**")
     try:
-        query = message.text.split(None, 1)[1]
-    except IndexError:
-        return await message.reply("❍ ᴘʀᴏᴠɪᴅᴇ ᴀɴ ɪᴍᴀɢᴇ ǫᴜɪᴇʀʏ ᴛᴏ sᴇᴀʀᴄʜ!")
-
-    lim = findall(r"lim=\d+", query)
-    try:
-        lim = int(lim[0].replace("lim=", ""))
-        query = query.replace(f"lim={lim}", "")
-    except IndexError:
-        lim = 6  # Default limit to 6 images
-
-    download_dir = "downloads"
-
-    try:
-        downloader.download(query, limit=lim, output_dir=download_dir, adult_filter_off=True, force_replace=False, timeout=60)
-        images_dir = os.path.join(download_dir, query)
-        if not os.listdir(images_dir):
-            raise Exception("No images were downloaded.")
-        lst = [os.path.join(images_dir, img) for img in os.listdir(images_dir)][:lim]  # Ensure we only take the number of images specified by lim
+        await app.send_media_group(message.chat.id, media_group)
+        await messagesend.delete()
     except Exception as e:
-        return await message.reply(f"❍ ᴇʀʀᴏʀ ɪɴ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ɪᴍᴀɢᴇs: {e}")
-
-    msg = await message.reply("❍ ɴᴀɴᴄʏ ғɪɴᴅɪɴɢ ɪᴍᴀɢᴇs.....")
-
-    count = 0
-    for img in lst:
-        count += 1
-        await msg.edit(f"❍ ɴᴀɴᴄʏ ғɪɴᴅ {count} ɪᴍᴀɢᴇs.....")
-
-    try:
-        await app.send_media_group(
-            chat_id=chat_id,
-            media=[InputMediaPhoto(media=img) for img in lst],
-            reply_to_message_id=message.id
-        )
-        shutil.rmtree(images_dir)
-        await msg.delete()
-    except Exception as e:
-        await msg.delete()
-        return await message.reply(f"❍ ᴇʀʀᴏʀ ɪɴ sᴇɴᴅɪɴɢ ɪᴍᴀɢᴇs: {e}")
+        await messagesend.edit(e)
