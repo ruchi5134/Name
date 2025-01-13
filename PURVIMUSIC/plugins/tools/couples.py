42f628cbf44b833065343d5f3e4efca1ad3ec472
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2025-01-13 21:00:09 +0530

    Update couples.py

--- PURVIMUSIC/plugins/tools/couples.py
+++ PURVIMUSIC/plugins/tools/couples.py
@@ -15,7 +15,7 @@ POLICE = [
     [
         InlineKeyboardButton(
             text="|| ˹⍟ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ⍟˼ ||",
-            url=f"https://t.me/ImNancybot?startgroup=true",
+            url=f"https://t.me/LavyaMusicbot?startgroup=true",
         ),
     ],
 ]
