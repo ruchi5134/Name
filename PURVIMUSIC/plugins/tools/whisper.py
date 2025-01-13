d70edc3b584900a09b20c8c712e16124c95c63d7
𓄂️𝆺𝅥⃝☠️ ⃪ͥ͢ ᷟ 𝐌͢ꝛ♥️꯭⃝⃨⃜⃝𝐕𝐈𝐊𝐊𝐘༏⃪͢🔥᪵᪳❛𝆺꯭𝅥༎ࠫ <vikashyadav735556@gmail.com>
2025-01-13 20:56:56 +0530

    Update whisper.py

--- PURVIMUSIC/plugins/tools/whisper.py
+++ PURVIMUSIC/plugins/tools/whisper.py
@@ -18,8 +18,8 @@ async def _whisper(_, inline_query):
         mm = [
             InlineQueryResultArticle(
                 title="⍟ ᴄʟɪᴄᴋ-ʜᴇʀᴇ ⍟",
-                description=f"@Ishumusicsbot [ USERNAME | ID ] [ TEXT ]",
-                input_message_content=InputTextMessageContent(f"😍 Usage:\n\n@Ishumusicsbot [ USERNAME | ID ] [ TEXT ]"),
+                description=f"@LavyaMusicbot [ USERNAME | ID ] [ TEXT ]",
+                input_message_content=InputTextMessageContent(f"😍 Usage:\n\n@LavyaMusicbot [ USERNAME | ID ] [ TEXT ]"),
                 thumb_url="https://files.catbox.moe/o9kx98.jpg",
                 reply_markup=switch_btn
             )
@@ -110,7 +110,7 @@ async def in_help():
     answers = [
         InlineQueryResultArticle(
             title="⍟ ᴄʟɪᴄᴋ-ʜᴇʀᴇ ⍟",
-            description=f"@Ishumusicsbot [USERNAME | ID] [TEXT]",
+            description=f"@LavyaMusicbot [USERNAME | ID] [TEXT]",
             input_message_content=InputTextMessageContent(f"**📍Usage:**\n\n@Ishumusicsbot (Target Username or ID) (Your Message).\n\n**Example:**\n@Aradhya_music_bot @username I Wanna fuck You"),
             thumb_url="https://files.catbox.moe/o9kx98.jpg",
             reply_markup=switch_btn
