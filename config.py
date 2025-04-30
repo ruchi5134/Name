6415471aaecce80a26a0b4c95728dbd8db524f10
Sareeflonda88 <142931776+Sareeflonda88@users.noreply.github.com>
2025-04-30 10:35:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -67,8 +67,8 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/choudharykiduniya")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/choudharykiduniya")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/choudhary13000")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/choudhary13000")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
