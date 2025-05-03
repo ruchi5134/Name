8a384fcda8310a5a14942eb4c558f60497874c62
Sareeflonda88 <142931776+Sareeflonda88@users.noreply.github.com>
2025-05-03 12:20:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,6 +31,9 @@ ASSUSERNAME = getenv("ASSUSERNAME")
 #---------------------------------------------------------------
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 #---------------------------------------------------------------
+# Vars For API End Pont.
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://tubed.okflix.top') ## E.G https://yt.okflix.
+YT_API_KEY = "sparrow123"
 #---------------------------------------------------------------
 
 # ----------------------------------------------------------------
